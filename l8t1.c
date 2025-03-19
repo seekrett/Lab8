49c4303498cdc2fe28577d12d09fe2adfb8a4a53
Yuto <63772196+seekrett@users.noreply.github.com>
2025-03-19 13:53:59 -0400

    initial commit

---
+++ l8t1.c
@@ -0,0 +1,9 @@
+// This program constructs a hash table.
+
+// include statements
+
+// Define {key, value} structure. (Define key and value as char datatype)
+typedef struct {
+    char key;
+    char value;
+} KeyValuePair;
