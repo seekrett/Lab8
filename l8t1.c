a41e66ed22e027de55d90acb144fc2071f2d95cd
Yuto <63772196+seekrett@users.noreply.github.com>
2025-03-19 14:41:10 -0400

    created HashTable structure

--- l8t1.c
+++ l8t1.c
@@ -7,3 +7,13 @@ typedef struct {
     char key;
     char value;
 } KeyValuePair;
+
+// Define the Hash Table structure. [Hint: a hash table is an array of {key, value} pairs]
+typedef struct {
+    KeyValuePair** items;
+    int size;
+    int count;
+} HashTable;
+
+// Create {key, value} pairs. [Hint: you need to allocate memory for key and value
+// and return a pointer to the {key, value} pair]
