c6fee277b6e2a472044d6211efeb43b83b07773b
Yuto <63772196+seekrett@users.noreply.github.com>
2025-03-22 14:51:10 -0400

    created freePair()

--- l8t1.c
+++ l8t1.c
@@ -1,19 +1,60 @@
 // This program constructs a hash table.
 
 // include statements
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-// Define {key, value} structure. (Define key and value as char datatype)
+#define TABLESIZE 100
+
+// Define {key, value} structure
 typedef struct {
+    // attributes
     char key;
     char value;
 } KeyValuePair;
+// name of struct
 
-// Define the Hash Table structure. [Hint: a hash table is an array of {key, value} pairs]
+// Define the Hash Table structure
 typedef struct {
+    // attributes
     KeyValuePair** items;
     int size;
     int count;
 } HashTable;
+// name of struct
 
-// Create {key, value} pairs. [Hint: you need to allocate memory for key and value
-// and return a pointer to the {key, value} pair]
+// Create {key, value} pairs
+KeyValuePair* createKeyValuePair(const char* key, const char* value) {
+    // allocate memory
+    KeyValuePair* pair = (KeyValuePair*) malloc(sizeof(KeyValuePair));
+
+    // similar to class.attribute
+    pair->key = strdup(key);
+    pair->value = strdup(value);
+    return pair;
+}
+
+// Create the Hash Table. [Hint: you need to allocate memory for the Hash Table, and set all {key, value}
+// pairs to NULL since they are not used) 
+HashTable* createHashTable() {
+    // allocate memory
+    HashTable* hashtable = (HashTable*) malloc(sizeof(HashTable));
+
+    // set all pairs to NULL
+    for (int i = 0; i < TABLESIZE; i++) {
+        hashtable->items[i] = NULL;
+    }
+
+    return hashtable;
+}
+
+// Define a function to free up the memory of a {key, value} pair. 
+void freePair(KeyValuePair* pair) {
+    // check if NULL
+    if (pair) {
+        free(pair->key);
+        free(pair->value);
+        free(pair);
+    }
+}
